57f8cec5c0f652a7f45024bb2ea920bb54c6be1e
JRI23 <joraijayden@outlook.com>
2022-01-04 16:03:01 +0100

    Created a beginning for the header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './sass/_global.scss';
 import Header from "./components/header/header";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
     return (
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,13 +4,19 @@ import './header.scss';
 const Header = () => {
     return (
         <>
-            <nav>
-                <ul className="d-flex topnav gap-2">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Work</a></li>
-                </ul>
+            <nav className="custom-navbar">
+                <div className="max-width d-flex align-items-center justify-content-between">
+                    <div className="logojorai"><a href="/">J<span>JDesign.</span></a></div>
+                    <ul className="link-list d-flex justify-content-around gap-3 mb-0">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Work</a></li>
+                        <li><a href="/">Skills</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </div>
             </nav>
+
         </>
     )
 }
